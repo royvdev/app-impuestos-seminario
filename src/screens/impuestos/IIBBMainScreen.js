daac05b22be1a850f687508015aae03f326fc7ba
Roy <royfvicente@gmail.com>
2022-07-04 00:32:54 -0300

    Add functions to create li from array

--- src/screens/impuestos/IIBBMainScreen.js
+++ src/screens/impuestos/IIBBMainScreen.js
@@ -6,26 +6,10 @@ import './IIBBMainScreen.css'
 function IIBBMainScreen() {
   const [impuestoData, setImpuestoData] = useState({});
 
-  console.log("Info:", impuestoData);
+  // console.log("Info:", impuestoData);
 
   /*  Esta pantalla es provisoria y debe ser reemplazada por la definitiva */
   function SeleccionPeriodoScreen() {
-    const yearList = [2019, 2020, 2021, 2022];
-    const monthList = [
-      "Enero",
-      "Febrero",
-      "Marzo",
-      "Abril",
-      "Mayo",
-      "Junio",
-      "Julio",
-      "Agosto",
-      "Septiembre",
-      "Octubre",
-      "Noviembre",
-      "Diciembre",
-    ];
-
 
     const handleChangePeriodo = (data) => {
       setImpuestoData({
@@ -33,26 +17,84 @@ function IIBBMainScreen() {
       });
     }
 
+    const dateList = [
+      { year: 2022, months: [ 
+                              // {name: "Diciembre", status: "noCompletado"},
+                              // {name: "Noviembre", status: "noCompletado"},
+                              // {name: "Octubre", status: "noCompletado"},
+                              // {name: "Septiembre", status: "noCompletado"},
+                              // {name: "Agosto", status: "noCompletado"},
+                              {name: "Julio", status: "porVencer"},
+                              {name: "Junio", status: "completado"},
+                              {name: "Mayo", status: "completado"},
+                              {name: "Abril", status: "completado"},
+                              {name: "Marzo", status: "completado"},
+                              {name: "Febrero", status: "completado"},
+                              {name: "Enero", status: "completado"} ] },
+
+      { year: 2021, months: [ {name: "Diciembre", status: "completado"},
+                              {name: "Noviembre", status: "completado"},
+                              {name: "Octubre", status: "completado"},
+                              {name: "Septiembre", status: "completado"},
+                              {name: "Agosto", status: "completado"},
+                              {name: "Julio", status: "completado"},
+                              {name: "Junio", status: "completado"},
+                              {name: "Mayo", status: "completado"},
+                              {name: "Abril", status: "completado"},
+                              {name: "Marzo", status: "completado"},
+                              {name: "Febrero", status: "completado"},
+                              {name: "Enero", status: "completado"} ] },
+
+      { year: 2020, months: [ {name: "Diciembre", status: "porVencer"},
+                              {name: "Noviembre", status: "porVencer"},
+                              {name: "Octubre", status: "porVencer"},
+                              {name: "Septiembre", status: "porVencer"},
+                              {name: "Agosto", status: "porVencer"},
+                              {name: "Julio", status: "porVencer"},
+                              {name: "Junio", status: "porVencer"},
+                              {name: "Mayo", status: "porVencer"},
+                              {name: "Abril", status: "porVencer"},
+                              {name: "Marzo", status: "porVencer"},
+                              {name: "Febrero", status: "porVencer"},
+                              {name: "Enero", status: "porVencer"} ] },
+                              
+      { year: 2019, months: [ {name: "Diciembre", status: "noCompletado"},
+                              {name: "Noviembre", status: "noCompletado"},
+                              {name: "Octubre", status: "noCompletado"},
+                              {name: "Septiembre", status: "noCompletado"},
+                              {name: "Agosto", status: "noCompletado"},
+                              {name: "Julio", status: "porVencer"},
+                              {name: "Junio", status: "completado"},
+                              {name: "Mayo", status: "completado"},
+                              {name: "Abril", status: "completado"},
+                              {name: "Marzo", status: "completado"},
+                              {name: "Febrero", status: "completado"},
+                              {name: "Enero", status: "completado"} ] },                              
+    ];
+
+    const yearList = [];
+    const monthList = [];
+
+    dateList.forEach((date) => {
+      yearList.push(<li className="row year" onClick={ console.log("testing onClick") }>{date.year}</li>)
+    });
+
+    dateList[0].months.forEach((month) => {
+      monthList.push(<li className={"row " + month.status}>{month.name}</li>)
+    });
+
     return (
       <React.Fragment>
         <h2>Seleccione un periodo</h2>
-        <div class="datePicker">
-          <div class="yearSelector">
+        <div className="datePicker">
+          <div className="yearSelector">
             <ul>
-              <li class="row month">{yearList[3]}</li>
-              <li class="row month">{yearList[2]}</li>
-              <li class="row month">{yearList[1]}</li>
-              <li class="row month">{yearList[0]}</li>
+              { yearList }
             </ul>
           </div>
-          <div class="monthSelector">
+          <div className="monthSelector">
             <ul>
-              <li class="row noCompletado">{monthList[5]}</li>
-              <li class="row porVencer">{monthList[4]}</li>
-              <li class="row completado">{monthList[3]}</li>
-              <li class="row completado">{monthList[2]}</li>
-              <li class="row completado">{monthList[1]}</li>
-              <li class="row completado">{monthList[0]}</li>
+              { monthList }
             </ul>
           </div>
         </div>
